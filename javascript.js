47c4c553cf9612fabdcde1eb37704a7dce01b133
Julianne Arroyo <JNARROYO26@YAHOO.COM>
2018-04-15 21:26:11 -0700

    moving
    MOving things around needlessly.
    late night...

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
 var styles=[
 'Realism', 'Impressionism', 'Abstract', 'Art Deco', 'Cubism', 'Surrealism',
-'Pop Art', 'Modern', 'Contemporary', 'Fantasy', 'Graffiti']
+'Pop Art', 'Modern', 'Contemporary', 'Fantasy', 'Graffiti', 'Continuous Line', 'Blind']
 
 function newStyle(){
   var randomNumber = Math.floor(Math.random() * (styles.length));
   document.getElementById('styleDisplay').innerHTML = styles[randomNumber];
+}
+
+
+var subjects=[
+'Seascape', 'Landscape', 'Cityscape', 'Meteor', 'Pineapple', 'Your Shoes',
+'Space Ship', 'Dinosaur', 'Big Ass Bird', 'Last Food You Ate', 'Favorite Celebrity']
+
+function newSubject(){
+  var randomNumber = Math.floor(Math.random() * (subjects.length));
+  document.getElementById('subjectDisplay').innerHTML = subjects[randomNumber];
 }
