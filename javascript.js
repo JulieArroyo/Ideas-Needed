9e1c29ffd5075e7411eaa9f98c4909648f4ea3ff
Julianne Arroyo <JNARROYO26@YAHOO.COM>
2018-04-16 21:42:07 -0700

    uniform the code
    added animals to the list
    added more space to the buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+// Animals
+var animals=[
+  'Skunk', 'Racoon', 'Elephant', 'Turkey', 'Walrus', 'Dog', 'Bear', 'Penguin', 'Owl','frog', 'Beaver',
+  'Fox', 'Cat', 'Ant', 'Bee','Fish', 'Caterpillar', 'Dolphin', 'Chicken', 'Shark', 'Mouse', 'Octopus',
+  'Lion', 'Parrot', 'Crab', 'Seal', 'Goose', 'Whale','Goat', 'Lizard', 'Panda', 'Dinosaur','Hippo',
+  'Hedgehog', 'Turtle', 'Fly', 'Peacock', 'Pig', 'Otter',]
+
+
+
+function newAnimal(){
+  var randomNumber = Math.floor(Math.random() * (animals.length));
+  document.getElementById('animalDisplay').innerHTML = animals[randomNumber]
+}
+
+// professions
+var professions=[
+  'Theif', 'Doctor', 'Samurai', 'Super Hero', 'Detective'
+]
+
+function newProfession(){
+  var randomNumber = Math.floor(Math.random() * (professions.length));
+  document.getElementById('professionDisplay').innerHTML = professions[randomNumber]
+}
+
+
+// Styles of Art
 var styles=[
 'Realism', 'Impressionism', 'Abstract', 'Art Deco', 'Cubism', 'Surrealism',
 'Pop Art', 'Modern', 'Contemporary', 'Fantasy', 'Graffiti', 'Continuous Line', 'Blind']
@@ -7,7 +33,7 @@ function newStyle(){
   document.getElementById('styleDisplay').innerHTML = styles[randomNumber];
 }
 
-
+// Subjects of art
 var subjects=[
 'Seascape', 'Landscape', 'Cityscape', 'Meteor', 'Pineapple', 'Your Shoes',
 'Space Ship', 'Dinosaur', 'Big Ass Bird', 'Last Food You Ate', 'Favorite Celebrity']
